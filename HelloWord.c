bfe4dffa5910949ddceec494ca1f481ca04c9af6
jhonatasal <jhonatasa_s@hotmail.com>
2020-04-01 10:49:30 -0300

    Hellow world

---
+++ HelloWord.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//funcao principal do programa
+
+int main()
+{
+
+	//Imprime na tela
+	printf("Oi mundo! \n");
+
+	//Pausa o programa apos executar
+	system("pause");
+
+	//retorna o valor 0
+	return 0;
+
+}
