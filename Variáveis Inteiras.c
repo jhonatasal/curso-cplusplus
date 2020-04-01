30aab0f2afaac8f313bd2dc92db50cb43834623f
jhonatasal <jhonatasa_s@hotmail.com>
2020-04-01 11:38:10 -0300

    Variaveis Inteiras

---
+++ Variáveis Inteiras.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	int a = 5 , b;
+
+	printf("%d", a );
+
+	//pula linha
+	printf("\n");
+
+	//concantenando
+	printf("O valor da var a eh  %d", a);
+
+	printf("\n");
+	//lendo valores
+	scanf("%d", &b);
+
+	printf("\n");
+
+	printf("O valor da var b eh  %d", b);
+
+
+	printf("\n");
+	printf("Uhuu");
+
+
+	system("pause");
+	return 0;
+}
